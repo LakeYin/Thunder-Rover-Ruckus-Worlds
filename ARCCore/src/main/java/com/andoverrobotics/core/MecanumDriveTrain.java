cf8f06b02a443faedd8f845e654db1e625781f0f
Lake Yin <la11010e@gmail.com>
2018-05-23 16:38:43 -0400

    Added some comments

--- ARCCore/src/main/java/com/andoverrobotics/core/MecanumDriveTrain.java
+++ ARCCore/src/main/java/com/andoverrobotics/core/MecanumDriveTrain.java
@@ -6,20 +6,27 @@ package com.andoverrobotics.core;
 
 public interface MecanumDriveTrain extends DriveTrain {
 
+    // [-1, 1]
     void strafeRight(double distanceInInches);
     void strafeRight(double distanceInInches, double power);
 
+    // [-1, 1]
     void strafeLeft(double distanceInInches);
     void strafeLeft(double distanceInInches, double power);
 
+    // [-1, 1]
     void strafeToCoordinate(double xInInches, double yInInches);
     void strafeToCoordinate(double xInInches, double yInInches, double power);
 
+    // [0, 360]
+    // [-1, 1]
     void strafeDegrees(int degrees, double distanceInInches);
     void strafeDegrees(int degrees, double distanceInInches, double power);
 
     // -- Teleop Methods --
 
+    // [0, 360]
+    // [-1, 1]
     void setDegreeOfStrafe(int degrees);
     void setDegreeOfStrafe(int degrees, double power);
 }
