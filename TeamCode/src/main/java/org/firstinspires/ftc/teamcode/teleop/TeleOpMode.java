6d5238f313203768d9ce1ad7e2feb27639a0dff0
Michael Peng <michael@mcmoo.org>
2018-10-12 14:31:09 -0400

    Add team code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpMode.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOpMode extends OpMode {
+
+  private TeleOpBot bot;
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void loop() {
+
+  }
+}
