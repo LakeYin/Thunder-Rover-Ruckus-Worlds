6c43f4819e39802a34ca4cec1bd754a44b66772a
Michael P <michael@mcmoo.org>
2018-10-12 12:01:23 -0400

    Incorporate changes from ARC-Core again (#2)
    * Updated the .gitignore
    
    More useful .gitignore generated for Windows and Android Studio using gitignore.io
    
    https://www.gitignore.io/
    
    * Added macOS .gitignore settings
    
    Used gitignore.io again
    
    * Fixed and updated all of the gradle stuff to comply with the newer Android Studio versions
    
    * Removing .idea syncing (1/2)
    
    * Stop .idea syncing (Part 2/2)
    
    * Added the DogeCV 2018 code as well as the DogeCV examples
    
    * Stop .idea syncing(1/2)
    
    * Changed the formatting to be more correct
    
    * Fixed build.gradle
    
    * Remove Lightning code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousBot.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.autonomous;
-
-import org.firstinspires.ftc.teamcode.Bot;
-
-public class AutonomousBot extends Bot {
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpBot.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.teleop;
-
-import org.firstinspires.ftc.teamcode.Bot;
-
-public class TeleOpBot extends Bot {
-
-}
